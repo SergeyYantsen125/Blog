de699c0458506afd62216bcaa23af6334b27e4a1
Sergey <Serega575719@gmail.com>
2022-05-16 23:54:35 +0300

    4

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path('category/', views.Category_list.as_view()),
     path('posts/', views.Posts_List.as_view()),
-    path('post/created', views.PostCreatedView.as_view()),
+    path('post/created/', views.PostCreatedView.as_view()),
     path('post/<str:url>/', views.Post_detail.as_view()),
     path('comment/', views.Comment_create.as_view()),
     path('like/', views.Like_create.as_view()),
